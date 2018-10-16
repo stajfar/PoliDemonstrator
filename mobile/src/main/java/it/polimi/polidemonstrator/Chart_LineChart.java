0948626a526189cf378a1dbbb470e0c0ef553fd6
stajfr <stajfar@yahoo.com>
2018-10-15 21:49:31 -0700

    file edited by editor

--- mobile/src/main/java/it/polimi/polidemonstrator/Chart_LineChart.java
+++ mobile/src/main/java/it/polimi/polidemonstrator/Chart_LineChart.java
@@ -69,7 +69,7 @@ public class Chart_LineChart extends AppCompatActivity
 
 
 
-
+//this file updated by editor
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
